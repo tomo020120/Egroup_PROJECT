99f66b59f9eb526c72991a0b5b84ee9eaabc4476
tomo020120 <73629880+tomo020120@users.noreply.github.com>
2021-11-10 14:38:05 +0900

    mySQL用汎用クラス

---
+++ src/dao/sql/MySQLConnector.java
@@ -0,0 +1,58 @@
+package dao.sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class MySQLConnector {
+	private static Connection cn = null;
+
+	public static Connection getConnection() {
+		try {
+			if(cn == null) {
+				synchronized(MySQLConnector.class) {
+					if(cn == null) {
+						cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/nozawa?characterEncoding=UTF-8&serverTimezone=JST","infox","prox");
+						cn.setAutoCommit(false);
+					}
+				}
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		return cn;
+	}
+
+	public static void commitTransaction() {
+		try {
+			cn.commit();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void closeTransaction() {
+		try {
+			cn.close();
+			cn = null;
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void rollbackTransaction() {
+		try {
+			cn.rollback();
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
