0f8aad4e06785dd361e7a16a22283a8ec270ed36
22203094941 <73629730+22203094941@users.noreply.github.com>
2022-01-26 15:29:31 +0900

    ma

--- WebContent/js/productsScript.js
+++ WebContent/js/productsScript.js
@@ -1,17 +1,14 @@
 function bt(i){
 
     if(i.style.backgroundColor ==""){
-    	window.location.href = "addFavorite?itemId=" + i.value;
-
-
+    	//window.history.back(-1);
     	i.style.backgroundColor="#FFD700";
-    	window.history.back(-1);
-    }
-
-    if(i.style.backgroundColor =="#FFD700"){
+    	window.location.href = "addFavorite?itemId=" + i.value;
+    }else{
         i.style.backgroundColor="";
 
     	window.location.href = "deleteFavorite?itemId="+ i.value;
+
     }
 
 }
